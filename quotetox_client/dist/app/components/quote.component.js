2285a28d9df6d7078316818427badc92c3b77235
= <hmgaar@gmail.com>
2016-07-01 00:39:47 -0400

    added parse

--- quotetox_client/dist/app/components/quote.component.js
+++ quotetox_client/dist/app/components/quote.component.js
@@ -23,6 +23,7 @@ var QuoteComponent = (function () {
         this.today = new Date();
         this.last_drink = new Date(2016, 0, 17);
         this.alcohol_free_days = Math.floor(((this.today - this.last_drink) / (864 * Math.pow(10, 5))));
+        console.log("this.today: ", this.today);
         this.quote = this.quote_service.get_quotes_dict();
     };
     QuoteComponent.prototype.get_quotes = function () {
--- quotetox_client/src/app/components/quote.component.ts
+++ quotetox_client/src/app/components/quote.component.ts
@@ -29,6 +29,7 @@ export class QuoteComponent implements OnInit {
     this.today = new Date();
     this.last_drink = new Date(2016,0,17);
     this.alcohol_free_days = Math.floor(((this.today - this.last_drink)/(864*Math.pow(10,5))));
+    console.log("this.today: ", this.today);
     this.quote = this.quote_service.get_quotes_dict();
   }
 
